aa6972f8c9cb946dd814d296aeecfa66ccb0d750
Rufatsb <rufatsb@code.edu.az>
2022-12-02 04:00:14 +0400

    NSubtitute dll added and set to edit and play mode test dlls

---
+++ 2D Game Test/Assets/_GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        //movement
+        //atack
+        //health
+        //flip
+        //jump or double jump
+        //collect
+        //animation
+    }
+
+    void Update()
+    {
+        
+    }
+}

cd4587982fa0a827a1d6a2a39941df96d70d0379
Rufatsb <rufatsb@code.edu.az>
2022-12-04 03:15:26 +0400

    Player Movement PlayMode Test done

--- 2D Game Test/Assets/_GameFolders/Scripts/Tests/PlayModeTests/player_movement.cs
+++ 2D Game Test/Assets/_GameFolders/Scripts/Tests/PlayModeTests/player_movement.cs
@@ -2,41 +2,93 @@ using NSubstitute;
 using NUnit.Framework;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
+using UnityTddBeginner.Abstracts.Controllers;
 using UnityTddBeginner.Abstracts.Inputs;
 using UnityTddBeginner.Concretes.Controllers;
 
 namespace Movements
 {
-    public class player_movement : MonoBehaviour
+    public class player_movement
     {
+        IPlayerController _playerController;
+
+        private IEnumerator LoadPlayerMoveTestScene()
+        {
+            yield return SceneManager.LoadSceneAsync("PlayerMovementTest");
+
+        }
+
+        [UnitySetUp]
+        IEnumerator  Setup()
+        {
+            yield return LoadPlayerMoveTestScene();
+
+            _playerController = GameObject.FindObjectOfType<PlayerController>();
+            _playerController.InputReader = Substitute.For<IInputReader>();
+
+
+        }
+
+
+
         [UnityTest]
         [TestCase(1f,ExpectedResult =(IEnumerator)null)]
         [TestCase(-1f, ExpectedResult = (IEnumerator)null)]
-
-
         public IEnumerator PlayerMoveLeftOrRightNotEqualStartPosition(float inputValue)
         {
             //Arrange//
-
-            GameObject playerObject = new GameObject(name:"Player");
-
-            PlayerController playerController = playerObject.AddComponent<PlayerController>();
-
-            playerController.InputReader = Substitute.For<IInputReader>();
-
-            Vector3 startPosition = playerController.transform.position;
+            Vector3 startPosition = _playerController.transform.position;
 
             //Act//
 
-            playerController.InputReader.Horizontal.Returns(inputValue);
-
+            _playerController.InputReader.Horizontal.Returns(inputValue);
             yield return new WaitForSeconds(3F);
 
 
 
             //Assert//
-            Assert.AreNotEqual(startPosition, playerObject.gameObject.transform.position);
+
+            Assert.AreNotEqual(startPosition, _playerController.transform.position);
+        }
+
+        [UnityTest]
+      
+        public IEnumerator PlayerMoveRightEndPositionGreaterThanStartPosition()
+        {
+            //Arrange//
+            Vector3 startPosition = _playerController.transform.position;
+
+            //Act//
+
+            _playerController.InputReader.Horizontal.Returns(1f);
+            yield return new WaitForSeconds(3F);
+
+
+
+            //Assert//
+
+            Assert.Greater(_playerController.transform.position.x, startPosition.x);
+        }
+
+        [UnityTest]
+
+        public IEnumerator PlayerMoveLefttEndPositionGreaterThanStartPosition()
+        {
+            //Arrange//
+            Vector3 startPosition = _playerController.transform.position;
+
+            //Act//
+
+            _playerController.InputReader.Horizontal.Returns(-1f);
+            yield return new WaitForSeconds(3F);
+
+
+
+            //Assert//
+
+            Assert.Less(_playerController.transform.position.x, startPosition.x);
         }
     }
 
